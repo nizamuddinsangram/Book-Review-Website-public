d58a1d227b7fccb7042a1d4a1fabc0e67f8298f9
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-03-27 00:30:48 +0600

    show blog data

--- src/Component/BlogCard.jsx
+++ src/Component/BlogCard.jsx
@@ -1,28 +1,25 @@
-const BlogCard = () => {
+const BlogCard = ({ data }) => {
+  const { id, date, image, heading, description } = data;
+
   return (
-    <div>
-      <a
-        rel="noopener noreferrer"
-        href="#"
-        className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900"
-      >
-        <img
-          src="https://source.unsplash.com/random/480x360"
-          alt=""
-          className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500"
-        />
-        <div className="p-6 space-y-2 lg:col-span-5">
-          <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
-            Noster tincidunt reprimique ad pro
-          </h3>
-          <span className="text-xs text-gray-400">February 19, 2021</span>
-          <p>
-            Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in
-            graece fuisset, eos affert putent doctus id.
-          </p>
-        </div>
-      </a>
-    </div>
+    <a
+      rel="noopener noreferrer"
+      href="#"
+      className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900"
+    >
+      <img
+        role="presentation"
+        className="object-cover w-full rounded h-44 bg-gray-500"
+        src={image}
+      />
+      <div className="p-6 space-y-2">
+        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+          {heading}
+        </h3>
+        <span className="text-xs text-gray-400">January 21, 2021</span>
+        <p>{description}</p>
+      </div>
+    </a>
   );
 };
 
--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,33 +1,36 @@
+import { useLoaderData } from "react-router-dom";
+import BlogCard from "../../Component/BlogCard";
+
 const Blog = () => {
+  const data = useLoaderData();
+  console.log("data", data);
   return (
     <section className="bg-gray-800 text-gray-100 rounded-lg">
       <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-        //
+        <a
+          rel="noopener noreferrer"
+          href="#"
+          className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900"
+        >
+          <img
+            src={data[0]?.image}
+            alt=""
+            className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500"
+          />
+          <div className="p-6 space-y-2 lg:col-span-5">
+            <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
+              {data[0]?.heading}
+            </h3>
+            <span className="text-xs text-gray-400">{data[0]?.date}</span>
+            <p>{data[0]?.description}</p>
+          </div>
+        </a>
         <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900"
-          >
-            <img
-              role="presentation"
-              className="object-cover w-full rounded h-44 bg-gray-500"
-              src="https://source.unsplash.com/random/480x360?1"
-            />
-            <div className="p-6 space-y-2">
-              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className="text-xs text-gray-400">January 21, 2021</span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
-            </div>
-          </a>
+          {data?.map((data) => (
+            <BlogCard data={data} key={data.id} />
+          ))}
         </div>
+
         <div className="flex justify-center">
           <button
             type="button"
