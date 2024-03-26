9c01bb6367119eb549863d63d2d2b355ab33889e
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-03-26 15:21:29 +0600

    banner design fixed

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -2,27 +2,27 @@ import { Link } from "react-router-dom";
 import banner from "../../public/img7.avif";
 const Banner = () => {
   return (
-    <div className="flex items-center justify-center h-[70vh] bg-gray-200">
-      <div className="flex w-full max-w-7xl mx-auto  rounded-lg  overflow-hidden">
+    <div className="  lg:flex lg:items-center rounded-lg lg:justify-center  h-[70vh] bg-gray-200 ">
+      <div className="sm:grid sm:grid-cols-2 lg:flex  w-full max-w-7xl mx-auto  rounded-lg  overflow-hidden">
         {/* Left Side (60%) */}
-        <div className="w-3/5  py-8 px-6 flex flex-col justify-center items-center">
-          <h2 className="text-3xl font-bold text-white mb-4 text-center">
-            Books to freshen <br />
-            up your bookshelf
+        <div className="w-full lg:w-3/5   py-8 px-6 flex flex-col justify-center items-center">
+          <h2 className="text-3xl lg:text-6xl  text-start font-medium mb-4 leading-snug		">
+            Books to freshen up <br />
+            your bookshelf
           </h2>
           <Link
             to="/listed-book"
-            className="bg-green-500 text-white font-semibold py-2 px-4 rounded hover:bg-green-600"
+            className="bg-green-500  text-left  text-white font-semibold py-2 px-4 rounded hover:bg-green-600"
           >
             View The List
           </Link>
         </div>
-        {/* Right Side (40%) */}
-        <div className="w-2/5">
+
+        <div className="w-full lg:w-2/5">
           <img
             src={banner}
             alt="Image"
-            className="w-full h-full object-cover"
+            className=" w-full h-full object-cover"
           />
         </div>
       </div>
