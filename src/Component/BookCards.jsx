8143db35cbcb69d99c0ce4268645674283a69619
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-03-26 16:19:30 +0600

    design home card

--- src/Component/BookCards.jsx
+++ src/Component/BookCards.jsx
@@ -3,27 +3,34 @@ import { Link } from "react-router-dom";
 const BookCards = ({ item }) => {
   const { image, tags, bookName, category, rating, author } = item;
   return (
-    <>
+    <div className="mb-10">
       <Link to={`/card/${item.bookId}`}>
         <div className="card  bg-base-100 shadow-xl">
           <figure className="px-10 pt-10">
-            <img src={image} alt="Shoes" className="rounded-xl h-96" />
+            <img src={image} alt="Shoes" className="rounded-xl h-72 w-full" />
           </figure>
           <div className="card-body  text-start">
-            <p>tags</p>
-            <h2 className="card-title">{bookName}</h2>
-            <p>{author}</p>
-            <div className="flex justify-between ">
-              <p>{category}</p>
-              <p>{rating}</p>
+            <p>
+              {tags?.map((tag, index) => (
+                <span
+                  key={index}
+                  className="bg-green-200 text-green-700 rounded-full px-2 py-1 text-sm mr-2 mb-2"
+                >
+                  {tag}
+                </span>
+              ))}
+            </p>
+            <h2 className="text-3xl font-semibold">{bookName}</h2>
+            <p className="text-lg font-semibold  text-gray-500">By: {author}</p>
+            <hr className=" my-2" />
+            <div className="flex justify-between items-center">
+              <p className="text-lg font-semibold text-gray-500">{category}</p>
+              <p className="text-lg font-semibold text-gray-500">{rating}</p>
             </div>
-            {/* <div className="card-actions">
-        <button className="btn btn-primary">Buy Now</button>
-      </div> */}
           </div>
         </div>
       </Link>
-    </>
+    </div>
   );
 };
 
