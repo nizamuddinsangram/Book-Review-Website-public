086ff46709d476a8f6b54331dd5c6d9a90fd4a02
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-03-27 06:31:43 +0600

    bookdetails design fixed

--- src/Component/BookDetails.jsx
+++ src/Component/BookDetails.jsx
@@ -47,7 +47,7 @@ const BookDetails = () => {
   };
   return (
     <div>
-      <div className="sm:grid  grid-cols-1 lg:flex lg:justify-between lg:items-center rounded-lg  my-10 h-[80vh] lg:mt-16 lg:px-12 mb-20">
+      <div className="sm:grid  grid-cols-1 lg:flex lg:justify-between lg:items-center rounded-lg  my-10 lg:h-[80vh] lg:mt-16 lg:px-12 mb-10 lg:mb-20">
         {/* Left Side (Image) */}
         <div className="w-full   lg:w-1/2 pr-4">
           <img
@@ -58,24 +58,25 @@ const BookDetails = () => {
         </div>
         {/* Right Side (Details) */}
         <div className="w-full lg:w-1/2 pl-4">
-          {/* Title */}
-          <h2 className="text-5xl font-semibold my-4">
+          <h2 className="text-2xl lg:text-5xl font-semibold my-2 lg:my-4">
             {singleData?.bookName}
           </h2>
           {/* Seller Name */}
-          <p className="text-gray-600 my-4 text-xl">
+          <p className="text-gray-600 my-2 lg:my-4 text-lg lg:text-xl">
             By: <span className="text-xl">{singleData?.author}</span>
           </p>
           {/* Rating and Reviews */}
-          <hr className="my-4" />
-          <p className="text-xl">{singleData?.category}</p>{" "}
-          <hr className="my-4" />
-          <p className="text-xl font-semibold my-10 leading-loose	">
+          <hr className="lg:my-4 my-2" />
+          <p className="text-lg lg:text-xl">{singleData?.category}</p>{" "}
+          <hr className="lg:my-4 my-2" />
+          <p className="text-xl font-semibold my-3 lg:my-10 leading-tight	 lg:leading-loose	">
             Reviews:{" "}
-            <span className="text-lg font-normal">{singleData?.review}</span>
+            <span className="text-sm lg:text-lg font-normal">
+              {singleData?.review}
+            </span>
           </p>
           {/* Tags */}
-          <p className="text-lg  font-bold mb-6">
+          <p className="text-sm lg:text-lg  font-bold mb-2 lg:mb-6">
             Tags{"  "}
             {singleData?.tags?.map((tag, index) => (
               <span
@@ -87,16 +88,16 @@ const BookDetails = () => {
             ))}
           </p>
           {/* Horizontal Line */}
-          <hr className="my-4" />
+          <hr className="my-1 lg:my-4" />
           {/* Additional Details */}
-          <div className="mb-6">
-            <p className="text-gray-600 my-2">
+          <div className="mb-2 lg:mb-6">
+            <p className="text-gray-600 my-1 lg:my-2">
               Number of Pages: <span>{singleData?.totalPages}</span>
             </p>
-            <p className="text-gray-600 my-2">
+            <p className="text-gray-600 my-1 lg:my-2">
               Publisher: <span>{singleData?.publisher}</span>{" "}
             </p>
-            <p className="text-gray-600 my-2">
+            <p className="text-gray-600 my-1 lg:my-2">
               Year of Publishing:<span>{singleData?.yearOfPublishing}</span>{" "}
             </p>
             <p className="text-gray-600">
@@ -107,7 +108,7 @@ const BookDetails = () => {
           <div className="flex">
             <button
               onClick={handleReadBooks}
-              className=" btn btn-outline  font-bold text-lg px-4 py-2 rounded-md mr-2"
+              className=" btn btn-outline  font-bold text-lg px-4 py-2 rounded-md mr-2 "
             >
               Read
             </button>
